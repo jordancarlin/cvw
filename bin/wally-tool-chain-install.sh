9d79f37144c52125e7cf93087b2d06afbe300f9a
Jordan Carlin <jordanmcarlin@gmail.com>
2024-07-19 19:39:21 -0700

    updates

--- bin/wally-tool-chain-install.sh
+++ bin/wally-tool-chain-install.sh
@@ -45,6 +45,7 @@ ENDC='\033[0m' # Reset to default color
 ## Helper functions
 # Error handler
 error() {
+    set +x
     echo -e "${FAIL_COLOR}Error: $STATUS installation failed"
     echo -e "Error on line ${BASH_LINENO[0]} with command $BASH_COMMAND${ENDC}"
     exit 1
